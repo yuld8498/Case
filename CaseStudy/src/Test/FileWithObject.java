bc8f81094cfc12f9769d915918af80653abf9fbc
yuld8498 <truongthanh11201@gmail.com>
2022-07-05 11:48:03 +0700

    add

--- CaseStudy/src/Test/FileWithObject.java
+++ CaseStudy/src/Test/FileWithObject.java
@@ -4,18 +4,21 @@ import tvt.libaryManagement.model.Product;
 import tvt.libaryManagement.service.ProductServer;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileWithObject {
     public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
         ProductServer productServer = new ProductServer();
-        for (Product product: productServer.findAll()){
+        for (Product product : productServer.findAll()) {
             System.out.println(product);
         }
         //trỏ đến file và ghi dữ liệu vào. dùng FileOutputStream để trỏ tới file, dùng ObjectOuputStream để tạo đối tượng ghi file.
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\ProductList.txt");
-            ObjectOutputStream objectOutputStream =new ObjectOutputStream(fileOutputStream);
-            for (Product product: productServer.findAll()){
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            for (Product product : productServer.findAll()) {
                 objectOutputStream.writeObject(product);
             }
             objectOutputStream.close();
@@ -26,16 +29,62 @@ public class FileWithObject {
         }
         //In ra các đối tượng trong file với InputStream.
         try {
-            FileInputStream fileInputStream =new FileInputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\ProductList.txt");
-            ObjectInputStream objectInputStream =new ObjectInputStream(fileInputStream);
+            FileInputStream fileInputStream = new FileInputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\ProductList.txt");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             System.out.println("Product List in file: ");
-            while (fileInputStream.available()>0){
+            while (fileInputStream.available() > 0) {
                 System.out.println(objectInputStream.readObject());
             }
             objectInputStream.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        ArrayList<Product> arrayList = new ArrayList<>(productServer.findAll());
+        writeProduct(arrayList);
+        readProduct();
+    }
+
+    //Đọc file thành list
+    public static void readProduct() {
+        ArrayList<Product> newArraylist = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\haha.txt");
+            Scanner scanner = new Scanner(fileInputStream);
+            while (scanner.hasNext()) {
+                Product product = new Product();
+                product.setProductID(Integer.parseInt(scanner.nextLine()));
+                product.setProductName(scanner.nextLine());
+                product.setAuthor(scanner.nextLine());
+                product.setCategory(scanner.nextLine());
+                product.setProductQuaility(Integer.parseInt(scanner.nextLine()));
+                product.setProductPrice(Double.parseDouble(scanner.nextLine()));
+                newArraylist.add(product);
+            }
+            System.out.println("Next time: ");
+            for (Product product : newArraylist) {
+                System.out.println(product.toString());
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //ghi list vào file
+    public static void writeProduct(ArrayList<Product> arrayList) {
+        try (PrintWriter printWriter = new PrintWriter("D:\\Module2\\Case\\CaseStudy\\src\\Test\\haha.txt")) {
+            for (Product product : arrayList) {
+                printWriter.println(product.getProductID());
+                printWriter.println(product.getProductName());
+                printWriter.println(product.getAuthor());
+                printWriter.println(product.getCategory());
+                printWriter.println(product.getProductQuaility());
+                printWriter.println(product.getProductPrice());
+            }
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
--- CaseStudy/src/Test/Read.java
+++ CaseStudy/src/Test/Read.java
@@ -4,24 +4,29 @@ import java.io.*;
 
 public class Read {
     public static void main(String[] args) {
-//        try {
-//            FileInputStream fileInputStream = new FileInputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\sinhvien.txt");
-//            BufferedInputStream dataInputStream = new BufferedInputStream(fileInputStream);
-//            System.out.println(dataInputStream.read());
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
         try {
-            FileReader fileReader = new FileReader("D:\\Module2\\Case\\CaseStudy\\src\\Test\\sinhvien.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            System.out.println(bufferedReader.readLine());
+            FileInputStream fileInputStream = new FileInputStream("D:\\Module2\\Case\\CaseStudy\\src\\Test\\sinhvien.txt");
+            BufferedInputStream dataInputStream = new BufferedInputStream(fileInputStream);
+//            System.out.println(dataInputStream.read());
+            int j = dataInputStream.available();
+            for (int i = 0; i < j; i++) {
+                System.out.println(dataInputStream.read());
+            }
+            dataInputStream.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+//        try {
+//            FileReader fileReader = new FileReader("D:\\Module2\\Case\\CaseStudy\\src\\Test\\sinhvien.txt");
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//            System.out.println(bufferedReader.readLine());
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 }
